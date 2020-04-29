9d2a0e6a88a1f80bd338b7774a7d9af94bc83ceb
Ankitsingh0695 <55825893+Ankitsingh0695@users.noreply.github.com>
2020-04-29 16:24:53 +0530

    Add files via upload

---
+++ CURD/src/main/java/p1/Main.java
@@ -0,0 +1,13 @@
+package p1;
+
+public class Main {
+
+	public static void main(String[] args) {
+			
+			Operations crudOperations = new Operations();
+			//crudOperations.insertEntity();
+			//crudOperations.findEntity();
+			crudOperations.updateEntity();
+			//crudOperations.removeEntity();
+	}
+}
